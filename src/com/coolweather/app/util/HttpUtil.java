86d286152801a7c1cd72b8d35278d4a77e619015
junqi <luke.li@tcl.com>
2016-09-13 20:32:28 +0800

    finish weather info

--- src/com/coolweather/app/util/HttpUtil.java
+++ src/com/coolweather/app/util/HttpUtil.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import android.util.Log;
+
 public class HttpUtil {
 	public static void sendHttpRequest(final String address,
 			final HttpCallbackListener listener) {
@@ -29,10 +31,12 @@ public class HttpUtil {
 							if (listener != null) {
 								// 回调onFinish()方法
 								listener.onFinish(response.toString());
+								Log.i("response", response.toString());
 							}
 						} catch (Exception e) {
 							if (listener != null) {
 								// 回调onError()方法
+							    e.printStackTrace();
 								listener.onError(e);
 							}
 						} finally {
